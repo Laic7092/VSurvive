c965c843e0ab137587ad1ecabc9d0bd6136262c2
Laix <894169287@qq.com>
2023-09-30 19:25:40 +0800

    update behaviour

---
+++ object-system/behaviour/behaviour.js
@@ -0,0 +1,35 @@
+import Component from "../component.js"
+
+class Behaviour extends Component{
+    constructor() {
+        super()
+        this.enabled = true
+        isActiveAndEnabled = true
+    }
+}
+
+export default class MonoBehaviour extends Behaviour {
+    constructor() {
+
+    }
+
+    awake() {
+
+    }
+
+    fixUpdate() {
+
+    }
+
+    lateUpdate() {
+
+    }
+
+    start() {
+
+    }
+
+    update() {
+
+    }
+}
