76dc5c6095b8796fdb01064acb91e6d26f4e7b14
Laix <894169287@qq.com>
2023-10-25 10:44:35 +0800

    optimize closure

--- game-engine/component-manager/transform.js
+++ game-engine/component-manager/transform.js
@@ -9,15 +9,34 @@ class Vector2 {
     }
 }
 
-class Transform extends Component{
+class Transform extends Component {
     position
     parent
 
-    constructor(gameobject, transform, { parent = null, position = new Vector2()}) {
+    constructor(gameobject, transform, { parent = null, position = new Vector2() }) {
         super('Transform', gameobject)
         this.transform = this
         this.position = position
         this.parent = parent
+    }
+
+    moveTowards(vector2, v) {
+        const { x, y } = vector2
+        this.position.x -= x * v
+        this.position.y -= y * v
+    }
+
+    getDistance(posiA, posiB) {
+        const xSub = posiA.x - posiB.x
+        const ySub = posiA.y - posiB.y
+        const powX = Math.pow(xSub, 2)
+        const powY = Math.pow(ySub, 2)
+        const d = Math.sqrt(powX + powY)
+        return {
+            d,
+            y: ySub / d,
+            x: xSub / d
+        }
     }
 }
 
--- game-engine/diy-script/monster.js
+++ game-engine/diy-script/monster.js
@@ -1,51 +1,30 @@
 import Component from "../component-manager/component.js"
+
 function monster(gameobject, transform) {
     const position = transform.position
-    const { x, y } = position
-    let speed = 10
+    let speed = 1
     let player = null
     let playerTransform = null
-    let playerPosition = null
-    function start() {
+    let playerPosition = null;
+
+    (function start() {
         player = gameobject.scene.getGameobject()
         playerTransform = player.transform
         playerPosition = playerTransform.position
-    }
-    start()
-
+    })()
 
     const update = () => {
-        const distance = getDistance(position, playerPosition)
-        if (distance.d > 5)
-            follow(distance)
-    }
-
-    function move(x, y) {
-        position.x -= x * speed
-        position.y -= y * speed
-    }
-
-    function follow({ sin, cos }) {
-        // console.log(moveV2);
-        // y = 单位长*ySub/d
-        move(cos, sin)
-    }
-
-    function getDistance(posiA, posiB) {
-        const xSub = posiA.x - posiB.x
-        const ySub = posiA.y - posiB.y
-        const powX = Math.pow(xSub, 2)
-        const powY = Math.pow(ySub, 2)
-        const d = Math.sqrt(powX + powY)
-        return {
-            d,
-            sin: ySub / d,
-            cos: xSub / d
+        const distance = transform.getDistance(position, playerPosition)
+        const { d, ...v2 } = distance
+        if (d > 5) {
+            transform.moveTowards(v2, speed)
         }
     }
 
     return update
 }
+
+
 
 class Monster extends Component {
     update
